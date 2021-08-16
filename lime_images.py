641e8fe28fbd5f65f4c4e27d3ed990245dcef6ae
Nino van Halem <na.vanhalem@gmail.com>
2021-08-16 09:22:33 +0200

    update network and gitignore.

--- lime_images.py
+++ lime_images.py
@@ -1,33 +1,34 @@
 import keras
 import numpy as np
-import tensorflow.keras.datasets.fashion_mnist as fashion_mnist
-from keras.layers import Conv2D, Dense, MaxPooling2D, Flatten, InputLayer
+import tensorflow.keras.datasets.cifar10 as cifar10
+from keras.layers import Conv2D, Dense, MaxPooling2D, Flatten, InputLayer, Dropout
 from keras.utils.np_utils import to_categorical
 
 
-def to_rgb(x):
-    x_rgb = np.zeros((x.shape[0], 28, 28, 3))
-    for i in range(3):
-        x_rgb[..., i] = x[..., 0]
-    return x_rgb
-
-
-(x_train, y_train), (x_test, y_test) = fashion_mnist.load_data()
-x_train = to_rgb(np.expand_dims(x_train.astype("float32") / 255, -1))
-x_test = to_rgb(np.expand_dims(x_test.astype("float32") / 255, -1))
+(x_train, y_train), (x_test, y_test) = cifar10.load_data()
+# x_train = np.expand_dims(x_train.astype("float32") / 255, -1)
+# x_test = np.expand_dims(x_test.astype("float32") / 255, -1)
 y_train = to_categorical(y_train, num_classes=10)
 y_test = to_categorical(y_test, num_classes=10)
 
 model = keras.Sequential()
-model.add(InputLayer(input_shape=(28, 28, 3,)))
-model.add(Conv2D(64, 5, padding='same', activation="relu"))
-model.add(MaxPooling2D(4))
+model.add(InputLayer(input_shape=(100, 100, 3,)))
+model.add(Conv2D(8, 5, padding='same', activation="relu"))
+model.add(MaxPooling2D(2))
+model.add(Conv2D(16, 3, padding='same', activation="relu"))
+model.add(MaxPooling2D(2))
 model.add(Conv2D(32, 3, padding='same', activation="relu"))
 model.add(MaxPooling2D(2))
 model.add(Flatten())
 model.add(Dense(32))
-model.add(Dense(10, activation='softmax'))
+model.add(Dropout(0.2))
+model.add(Dense(2, activation='softmax'))
 model.summary()
+
+
+
+
+
 
 model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
 model.fit(x_train, y_train, batch_size=16, epochs=2)
@@ -41,7 +42,7 @@ from skimage.segmentation import mark_boundaries
 from matplotlib import pyplot as plt
 
 temp_1, mask_1 = explanation.get_image_and_mask(explanation.top_labels[0], positive_only=True, num_features=5,
-                                                hide_rest=True)
+                                                hide_rest=False)
 temp_2, mask_2 = explanation.get_image_and_mask(explanation.top_labels[0], positive_only=False, num_features=10,
                                                 hide_rest=False)
 
