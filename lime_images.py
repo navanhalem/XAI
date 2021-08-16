f6eb8d719436efda2e0898f235bceb34f9e7f117
Nino van Halem <na.vanhalem@gmail.com>
2021-08-16 09:58:28 +0200

    added cat_image, updated network and gitignore.

--- lime_images.py
+++ lime_images.py
@@ -1,48 +1,34 @@
-import keras
 import numpy as np
-import tensorflow.keras.datasets.cifar10 as cifar10
-from keras.layers import Conv2D, Dense, MaxPooling2D, Flatten, InputLayer, Dropout
-from keras.utils.np_utils import to_categorical
+import skimage
+from skimage import io, transform
+from tensorflow.keras.applications import inception_v3 as inc_net
+from tensorflow.keras.applications.imagenet_utils import decode_predictions
 
 
-(x_train, y_train), (x_test, y_test) = cifar10.load_data()
-# x_train = np.expand_dims(x_train.astype("float32") / 255, -1)
-# x_test = np.expand_dims(x_test.astype("float32") / 255, -1)
-y_train = to_categorical(y_train, num_classes=10)
-y_test = to_categorical(y_test, num_classes=10)
-
-model = keras.Sequential()
-model.add(InputLayer(input_shape=(100, 100, 3,)))
-model.add(Conv2D(8, 5, padding='same', activation="relu"))
-model.add(MaxPooling2D(2))
-model.add(Conv2D(16, 3, padding='same', activation="relu"))
-model.add(MaxPooling2D(2))
-model.add(Conv2D(32, 3, padding='same', activation="relu"))
-model.add(MaxPooling2D(2))
-model.add(Flatten())
-model.add(Dense(32))
-model.add(Dropout(0.2))
-model.add(Dense(2, activation='softmax'))
-model.summary()
+def transform_img_fn_ori(url):
+    img = skimage.io.imread(url)
+    img = skimage.transform.resize(img, (299, 299))
+    img = (img - 0.5) * 2
+    img = np.expand_dims(img, axis=0)
+    preds = inet_model.predict(img)
+    for i in decode_predictions(preds)[0]:
+        print(i)
+    return img
 
 
-
-
-
-
-model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
-model.fit(x_train, y_train, batch_size=16, epochs=2)
+inet_model = inc_net.InceptionV3()
+images_inc_im = transform_img_fn_ori('cat_image.jpg')
 
 from lime import lime_image
-
-explainer = lime_image.LimeImageExplainer()
-explanation = explainer.explain_instance(x_test[0], model.predict, top_labels=3, num_samples=1000)
-
 from skimage.segmentation import mark_boundaries
 from matplotlib import pyplot as plt
 
+explainer = lime_image.LimeImageExplainer()
+explanation = explainer.explain_instance(images_inc_im[0].astype('double'), inet_model.predict,
+                                         top_labels=3, hide_color=0, num_samples=1000)
+
 temp_1, mask_1 = explanation.get_image_and_mask(explanation.top_labels[0], positive_only=True, num_features=5,
-                                                hide_rest=False)
+                                                hide_rest=True)
 temp_2, mask_2 = explanation.get_image_and_mask(explanation.top_labels[0], positive_only=False, num_features=10,
                                                 hide_rest=False)
 
